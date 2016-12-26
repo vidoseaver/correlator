1570d50127891d3dcf5e36f8b2052cbba0550069
Vido <vidoseaver@gmail.com>
2016-12-26 09:08:07 -0700

    Link for google verifaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'google_verifications/index'
+
+  get "/googlebf51e9d0ddcd799b.html", to: "google_verifications#index"
+
+
+
+
 end
