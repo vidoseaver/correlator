478a00d8e34bc83e21c038713b1a3e74afca2ae2
Vido <vidoseaver@gmail.com>
2016-12-26 09:17:38 -0700

    setup root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'google_verifications/index'
 
-  get "/googlebf51e9d0ddcd799b.html", to: "google_verifications#index"
+  get "/", to: "google_verifications#index"
 
 
 
