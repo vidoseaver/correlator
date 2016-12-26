bf9297c9050d8f1ce6eb59264e529da5f21b05c9
Vido <vidoseaver@gmail.com>
2016-12-26 15:17:58 -0700

    Json for all cities showing, show is working w/ id number

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::CitiesController < ApplicationController
   def index
+    render json: City.all
   end
 
   def show
+    render json: City.find_by(city_params)
   end
+
+  private
+    def city_params
+      params.permit(:id,:name)
+    end
+
 end
