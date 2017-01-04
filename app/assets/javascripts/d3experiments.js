9350921ffaa7075150cbd23b7a9cdb1d65500097
Vido <vidoseaver@gmail.com>
2017-01-03 17:50:50 -0700

    comment out experiments

--- app/assets/javascripts/d3experiments.js
+++ app/assets/javascripts/d3experiments.js
@@ -134,28 +134,28 @@
 
     // ________________________________
 
-    var canvas = d3.select('body').append('svg')
-      .attr('width', 300)
-      .attr('height', 500);
-
-    var data = [
-      {x: 10, y: 20}
-      {x: 40, y: 60}
-      {x: 50, y: 70}
-    ];
-
-    var group = canvas.append('g')
-      .attr('transform', 'translate(100, 1000)');
-
-    var line = d3.svg.line()
-      .x(function(d) { return d.x })
-      .y(function(d) { return d.y });
-
-    group.selectAll('path')
-      .data([data])
-      .enter()
-      .append('path')
-      .attr('d', line)
-      .attr('fill', 'none')
-      .attr('stroke', '#000')
-      .attr('stroke-width', 10)
+    // var canvas = d3.select('body').append('svg')
+    //   .attr('width', 300)
+    //   .attr('height', 500);
+    //
+    // var data = [
+    //   {x: 10, y: 20}
+    //   {x: 40, y: 60}
+    //   {x: 50, y: 70}
+    // ];
+    //
+    // var group = canvas.append('g')
+    //   .attr('transform', 'translate(100, 1000)');
+    //
+    // var line = d3.svg.line()
+    //   .x(function(d) { return d.x })
+    //   .y(function(d) { return d.y });
+    //
+    // group.selectAll('path')
+    //   .data([data])
+    //   .enter()
+    //   .append('path')
+    //   .attr('d', line)
+    //   .attr('fill', 'none')
+    //   .attr('stroke', '#000')
+    //   .attr('stroke-width', 10)
