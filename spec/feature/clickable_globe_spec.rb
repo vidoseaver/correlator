e831f85e0673a0b984ed8be4f60023b27bf9d415
KINAN WHYTE <kinanwhyte@gmail.com>
2017-01-11 20:48:29 -0700

    Add more tests

--- spec/feature/clickable_globe_spec.rb
+++ spec/feature/clickable_globe_spec.rb
@@ -17,6 +17,37 @@ describe 'home page' do
     end
   end
 
+  scenario "page has a header with styling" do
+    visit root_path
+
+    page.has_css?('header h1.app-title')
+  end
+
+  scenario "page has a globe and data container with styling" do
+    visit root_path
+
+    page.has_css?('body section.globe-and-data-container')
+  end
+
+  scenario "page has a country info container with styling" do
+    visit root_path
+
+    page.has_css?('body section#country_info_container')
+  end
+
+  scenario "page has a bar-graph with styling" do
+    visit root_path
+
+    page.has_css?('body div#bar-graph')
+  end
+
+  scenario "page has a glove nav container with styling" do
+    visit root_path
+
+    page.has_css?('body nav#globe_nav_container')
+  end
+
+
   scenario "show instructions-title when visiting home page" do
     visit root_path
 
@@ -29,7 +60,7 @@ describe 'home page' do
 
   scenario "user clicks a country and country data is rendered" do
     visit root_path
-    click_link ".clickable"
+    click_link(".clickable")
 
     within("#instructions-title") do
       expect(page).to have_content('Click a Country to View Its Data')
